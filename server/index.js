94921de6c57776dd43339df2bc7b26b4391bf71e
Шиянов Данила Андреевич <ShiynovDA@vsu.by>
2022-11-01 15:15:55 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+// подключение express, cors, mongoose
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// создаем объект приложения
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log("DB Connection Successful")
+}).catch((err)=>{
+  console.log(err.message);
+})
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
