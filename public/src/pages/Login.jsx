188c7cca5b06b1f600223535813aece97b61513e
Шиянов Данила Андреевич <ShiynovDA@vsu.by>
2022-11-03 15:48:09 +0300

    03.11.2022

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../assets/logo_chat.svg";
+import Logo from "../assets/logo_chat1.svg";
 
 function Login() {
   const handleSubmit = (event)=> {
@@ -17,7 +17,7 @@ function Login() {
         <div className='brand'>
           <img src={Logo} alt='Logo'/>
         </div>
-        
+        <hr/>
         <input type='number' placeholder='Логин' name='login' onChange={e=>handleChange(e)}/>
         <input type='password' placeholder='Пароль' name='password' onChange={e=>handleChange(e)}/>
 
@@ -27,71 +27,61 @@ function Login() {
   );
 }
 
-const FormContainer = styled.div`  height: 100vh;
-width: 100vw;
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 1rem;
-align-items: center;
-background-color: #131324;
-.brand {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  justify-content: center;
-  img {
-    height: 7rem;
-  }
-  h1 {
-    color: white;
-    text-transform: uppercase;
-  }
-}
-form {
+const FormContainer = styled.div` 
+  height: 100vh;
+  width: 100vw;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
-  background-color: #00000076;
-  border-radius: 2rem;
-  padding: 3rem 5rem;
-}
-input {
-  background-color: transparent;
-  padding: 1rem;
-  border: 0.1rem solid #4e0eff;
-  border-radius: 0.4rem;
-  color: white;
-  width: 100%;
-  font-size: 1rem;
-  &:focus {
-    border: 0.1rem solid #997af0;
-    outline: none;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  hr {
+    border: none;
+    border-top: 0.13rem solid #778899;
   }
-}
-button {
-  background-color: #4e0eff;
-  color: white;
-  padding: 1rem 2rem;
-  border: none;
-  font-weight: bold;
-  cursor: pointer;
-  border-radius: 0.4rem;
-  font-size: 1rem;
-  text-transform: uppercase;
-  &:hover {
-    background-color: #4e0eff;
+  .brand {
+    background-color: white;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 7rem;
+      margin-bottom: 10px;
+    }
   }
-}
-span {
-  color: white;
-  text-transform: uppercase;
-  a {
-    color: #4e0eff;
-    text-decoration: none;
+  form {
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: ;
+    border: 0.15rem solid #778899;
+    border-radius: 2rem;
+    padding: 2rem 3rem 1.9rem 3rem;
+  }
+  input {
+    backgound-color: #E8F0FE;
+    padding: 1rem;
+    border: 0.14rem solid #778899;
+    border-radius: 0.4rem;
+    width: 100%;
+    font-size: 1rem;
+  }
+  button {
+    background-color: #0F6FC5;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
     font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #00416A;
+    }
   }
-}
 `;
 
 export default Login;
--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -16,7 +16,7 @@ function Register() {
     <FormContainer>
       <form onSubmit={(event)=>handleSubmit(event)}>
         <div className='brand'>
-          {/* <img src={Logo} alt='Logo'/> */}
+          <img src={Logo} alt='Logo'/>
           <h1>snappy</h1>
         </div>
         
@@ -40,66 +40,66 @@ const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 4rem;
+  gap: 1rem;
   align-items: center;
-  //background-color: #131324;
-  // .brand {
-  //   display: flex;
-  //   align-items: center;
-  //   gap: 1rem;
-  //   justify-content: center;
-  //   img {
-  //     height: 5rem;
-  //   }
-  //   h1 {
-  //     color: white;
-  //     text-transform: uppercase;
-  //   }
-  // }
-  // form {
-  //   display: flex;
-  //   flex-direction: column;
-  //   gap: 2rem;
-  //   background-color: #00000076;
-  //   border-radius: 2rem;
-  //   padding: 3rem 5rem;
-  // }
-  // input {
-  //   background-color: transparent;
-  //   padding: 1rem;
-  //   border: 0.1rem solid #4e0eff;
-  //   border-radius: 0.4rem;
-  //   color: white;
-  //   width: 100%;
-  //   font-size: 1rem;
-  //   &:focus {
-  //     border: 0.1rem solid #997af0;
-  //     outline: none;
-  //   }
-  // }
-  // button {
-  //   background-color: #4e0eff;
-  //   color: white;
-  //   padding: 1rem 2rem;
-  //   border: none;
-  //   font-weight: bold;
-  //   cursor: pointer;
-  //   border-radius: 0.4rem;
-  //   font-size: 1rem;
-  //   text-transform: uppercase;
-  //   &:hover {
-  //     background-color: #4e0eff;
-  //   }
-  // }
-  // span {
-  //   color: white;
-  //   text-transform: uppercase;
-  //   a {
-  //     color: #4e0eff;
-  //     text-decoration: none;
-  //     font-weight: bold;
-  //   }
-  // }
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
 `;
 
 export default Register;
