cadb0e0d655d49bd82819f43947a89cf3e00eee7
Шиянов Данила Андреевич <ShiynovDA@vsu.by>
2022-11-04 14:15:33 +0300

    04.02.2022

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,20 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 
 function Register() {
+  
+  // const navigate = useNavigate();
+  // const toastOptions = {
+  //   position: "bottom-right",
+  //   autoClose: 8000,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   theme: "dark",
+  // };
 
-  const handleSubmit = (event)=> {
-    event.preventDefault();
-    alert("from");
-  };
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
 
-  const handleChange = (event) => {}
+  // useEffect(() => {
+  //   if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+  //     navigate("/");
+  //   }
+  // }, []);
 
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.name});
+  }
+
+  // const handleValidation = () => {
+  //   const { password, confirmPassword, username, email } = values;
+  //   if (password !== confirmPassword) {
+  //     toast.error(
+  //       "Password and confirm password should be same.",
+  //       toastOptions
+  //     );
+  //     return false;
+  //   } else if (username.length < 3) {
+  //     toast.error(
+  //       "Username should be greater than 3 characters.",
+  //       toastOptions
+  //     );
+  //     return false;
+  //   } else if (password.length < 8) {
+  //     toast.error(
+  //       "Password should be equal or greater than 8 characters.",
+  //       toastOptions
+  //     );
+  //     return false;
+  //   } else if (email === "") {
+  //     toast.error("Email is required.", toastOptions);
+  //     return false;
+  //   }
+
+  //   return true;
+  // };
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   if (handleValidation()) {
+  //     const { email, username, password } = values;
+  //     const { data } = await axios.post(registerRoute, {
+  //       username,
+  //       email,
+  //       password,
+  //     });
+
+  //     if (data.status === false) {
+  //       toast.error(data.msg, toastOptions);
+  //     }
+  //     if (data.status === true) {
+  //       localStorage.setItem(
+  //         process.env.REACT_APP_LOCALHOST_KEY,
+  //         JSON.stringify(data.user)
+  //       );
+  //       navigate("/");
+  //     }
+  //   }
+  // };
+
+  //onSubmit={(event)=>handleSubmit(event)}
   return (
     <FormContainer>
-      <form onSubmit={(event)=>handleSubmit(event)}>
+      <form>
         <div className='brand'>
           <img src={Logo} alt='Logo'/>
           <h1>snappy</h1>
