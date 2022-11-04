6a66162610a47614583df5ae9d76a7e6a58d251a
Шиянов Данила Андреевич <ShiynovDA@vsu.by>
2022-11-04 16:11:53 +0300

    04.11.2022

--- public/src/App.js
+++ public/src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
+
       </Routes>
     </BrowserRouter>
     );
--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -2,18 +2,27 @@ import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import styled from "styled-components";
+// import { useNavigate, Link } from "react-router-dom";
+// import Logo from "../assets/logo.svg";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
   
-  // const navigate = useNavigate();
-  // const toastOptions = {
-  //   position: "bottom-right",
-  //   autoClose: 8000,
-  //   pauseOnHover: true,
-  //   draggable: true,
-  //   theme: "dark",
-  // };
-
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
   const [values, setValues] = useState({
     username: "",
     email: "",
@@ -21,87 +30,73 @@ function Register() {
     confirmPassword: "",
   })
 
-  // useEffect(() => {
-  //   if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-  //     navigate("/");
-  //   }
-  // }, []);
-
   const handleChange = (event) => {
     setValues({ ...values, [event.target.name]: event.target.name});
   }
 
-  // const handleValidation = () => {
-  //   const { password, confirmPassword, username, email } = values;
-  //   if (password !== confirmPassword) {
-  //     toast.error(
-  //       "Password and confirm password should be same.",
-  //       toastOptions
-  //     );
-  //     return false;
-  //   } else if (username.length < 3) {
-  //     toast.error(
-  //       "Username should be greater than 3 characters.",
-  //       toastOptions
-  //     );
-  //     return false;
-  //   } else if (password.length < 8) {
-  //     toast.error(
-  //       "Password should be equal or greater than 8 characters.",
-  //       toastOptions
-  //     );
-  //     return false;
-  //   } else if (email === "") {
-  //     toast.error("Email is required.", toastOptions);
-  //     return false;
-  //   }
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );  
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+    return true;
+  };
 
-  //   return true;
-  // };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    handleValidation();
 
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   if (handleValidation()) {
-  //     const { email, username, password } = values;
-  //     const { data } = await axios.post(registerRoute, {
-  //       username,
-  //       email,
-  //       password,
-  //     });
+    // if (handleValidation()) {
+    //   const { email, username, password } = values;
+    //   const { data } = await axios.post(registerRoute, {
+    //     username,
+    //     email,
+    //     password,
+    //   });
+    // }
+  }
 
-  //     if (data.status === false) {
-  //       toast.error(data.msg, toastOptions);
-  //     }
-  //     if (data.status === true) {
-  //       localStorage.setItem(
-  //         process.env.REACT_APP_LOCALHOST_KEY,
-  //         JSON.stringify(data.user)
-  //       );
-  //       navigate("/");
-  //     }
-  //   }
-  // };
-
-  //onSubmit={(event)=>handleSubmit(event)}
   return (
-    <FormContainer>
-      <form>
-        <div className='brand'>
-          <img src={Logo} alt='Logo'/>
-          <h1>snappy</h1>
-        </div>
-        
-        <input type='text' placeholder='Username' name='username' onChange={e=>handleChange(e)}/>
-        <input type='email' placeholder='Email' name='email' onChange={e=>handleChange(e)}/>
-        <input type='password' placeholder='Password' name='password' onChange={e=>handleChange(e)}/>
-        <input type='password' placeholder='Confirm Password' name='confirm password' onChange={e=>handleChange(e)}/>
+    <>
+      <FormContainer>
+        <form onSubmit={(event)=>handleSubmit(event)}>
+          <div className='brand'>
+            <img src={Logo} alt='Logo'/>
+            <h1>snappy</h1>
+          </div>
+          
+          <input type='text' placeholder='Username' name='username' onChange={e=>handleChange(e)}/>
+          <input type='email' placeholder='Email' name='email' onChange={e=>handleChange(e)}/>
+          <input type='password' placeholder='Password' name='password' onChange={e=>handleChange(e)}/>
+          <input type='password' placeholder='Confirm Password' name='confirm password' onChange={e=>handleChange(e)}/>
 
-        <button type='submit'>Create User</button>
-        <span>
-          Already have an account ? <Link to="/login">Login</Link>
-        </span>
-      </form>
-    </FormContainer>
+          <button type='submit'>Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
   );
 }
 
